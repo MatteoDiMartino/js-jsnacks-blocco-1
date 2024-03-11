26741b8910207b7ceccea411eb3327bcbfef7f6e
MatteoDiMartino <matteodimartino359@gmail.com>
2024-03-11 14:18:05 +0100

    feat: add put prompt number with cicle for

--- snack-2/js/script.js
+++ snack-2/js/script.js
@@ -1,2 +1,7 @@
 // - Il software deve chiedere per 5 volte all’utente di inserire un numero.
 //   Il programma stampa la somma di tutti i numeri inseriti.
+
+for (let i = 0; i < 5; i++) {
+    let userNum = prompt('Inserisci un numero');
+    console.log (userNum);
+}

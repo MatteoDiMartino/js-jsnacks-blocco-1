73c702b2061ca44d02a5588b1f81f9bd3ee76a1c
MatteoDiMartino <matteodimartino359@gmail.com>
2024-03-11 18:48:03 +0100

    feat: add fix indent

--- snack-4/js/script.js
+++ snack-4/js/script.js
@@ -4,12 +4,10 @@
 
 let tenNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 let result = 0;
-// console.log (tenNum);
 
 for (let i = 0; i < 10; i++) {
     
     let contentAr = tenNum[i];
-    // console.log (contentAr);
 
 // sommo in maniera graduale
     result += contentAr;
